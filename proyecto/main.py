d02fea4bf99b040c6729b016a5d2a19a30ae6d12
GimenaIbarra <ibarra.gimena.abril@gmail.com>
2024-10-16 22:56:14 -0300

    agrego carpeta proyecto

---
+++ proyecto/main.py
@@ -0,0 +1,17 @@
+'''
+import funciones
+import funciones as fc
+fc.saludar()
+funciones.saludar("Hola div 112")
+'''
+
+'''
+from funciones import *
+
+saludar("Hola div")
+'''
+
+'''
+from funciones import saludar
+saludar("Hola")
+'''
